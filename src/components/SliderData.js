5f5c27d5330145c1d8dd4ff29fda4b814297e1e5
Kira <thiennguyen.fsd@gmail.com>
2021-10-21 09:09:34 +0700

    complete slider

---
+++ src/components/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+    {
+        image: "http://thuthuatphanmem.vn/uploads/2018/06/18/hinh-nen-laptop-4k-bai-bien-doc-dao-dep_034426429.jpeg",
+        alt: "image"
+    },
+    {
+        image: "http://thuthuatphanmem.vn/uploads/2018/06/18/hinh-nen-laptop-4k-canh-dong-co-xanh-muot_034426897.jpg",
+        alt: "image"
+    },
+    {
+        image: "http://thuthuatphanmem.vn/uploads/2018/06/18/hinh-nen-laptop-4k-canh-quan-thien-nhien-nhung-chu-buom-phat_ybaIe_034427099.jpg",
+        alt: "image"
+    },
+    {
+        image: "http://thuthuatphanmem.vn/uploads/2018/06/18/hinh-nen-laptop-4k-con-duong-doc-dao-cuc-dep_034429783.jpg",
+        alt: "image"
+    },
+    {
+        image: "http://thuthuatphanmem.vn/uploads/2018/06/18/hinh-nen-laptop-4k-con-duong-sat-dep_034430079.jpg",
+        alt: "image"
+    }
+]
